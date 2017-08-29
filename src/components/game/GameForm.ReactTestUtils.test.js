bbedb4ab54329e96e1acd7fc8c8c6d8512db5e17
John C <jcresswell@hotmail.com>
2017-08-28 21:50:20 -0500

    react tests finished

--- src/components/game/GameForm.ReactTestUtils.test.js
+++ src/components/game/GameForm.ReactTestUtils.test.js
@@ -5,9 +5,9 @@ import TestUtils from 'react-addons-test-utils';
 import GameForm from './GameForm';
 
 
-function setup() {
+function setup(saving) {
   let props = {
-    game: {}, saving: false, errors: {},
+    game: {}, saving: saving, errors: {},
     onSave: () => {},
     onChange: () => {}
   };
@@ -28,5 +28,21 @@ describe('GameForm via React Test Utils', () => {
     const { output } = setup();
 
     expect(output.type).toBe('form');
+    let [ h1 ] = output.props.children;
+
+    expect(h1.type).toBe('h1');
   });
+
+  it('save button is labeled "Save" when not saving', () => {
+    const { output } = setup(false);
+    const submitButton = output.props.children[4];
+    expect(submitButton.props.value).toBe('Save');
+  });
+
+  it('save button is labeled "Saving" when saving', () => {
+    const { output } = setup(true);
+    const submitButton = output.props.children[4];
+    expect(submitButton.props.value).toBe('Saving...');
+  });
+
 });
